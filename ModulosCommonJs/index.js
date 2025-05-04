e48295dc86f2440efe4e17322ee487a8bb9ffaea
anthonyguimas <anthonygabriel028@gmail.com>
2025-05-04 14:24:50 -0300

    modules

---
+++ ModulosCommonJs/index.js
@@ -0,0 +1,7 @@
+const render = require("./render.js");
+const store = require("./store.js");
+
+console.log("Iniciando a aplicação.");
+render();
+store();
+console.log("Aplicação finalizada.");
---
+++ tiposDeExportDoEsm/script.js
@@ -0,0 +1,10 @@
+import { inline } from "./inline.js";
+import defaultInLine from "./inline.js";
+import { group } from "./non-inline.js";
+import exportDefault from "./non-inline.js";
+
+inline();
+defaultInLine();
+
+group();
+exportDefault();
